16ac5750983bdfbeaddedf21080d2a795aabdffc
LakshanWC <lakshanwc@gmail.com>
2025-10-23 15:47:22 +0530

    updated endpoints
    updated endpoints to be more dynamic

--- python-backend/paper_analyzer/app/controller/feedback_controller.py
+++ python-backend/paper_analyzer/app/controller/feedback_controller.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter
 from app.service.feedback_service import get_all_feedback_for_user,get_all_feedback_subect_wise
 from app.service.feedback_service import get_all_subjects_in_feedbacks
@@ -5,7 +7,7 @@ from app.service.feedback_service import get_all_subjects_in_feedbacks
 router = APIRouter()
 
 @router.get("/feedbacks")
-async def get_all_feedbacks(userid:str):
+async def get_all_feedbacks(userid:Optional[str] = None):
     try:
         return get_all_feedback_for_user(userid)
     except Exception as error:
@@ -13,16 +15,15 @@ async def get_all_feedbacks(userid:str):
         return f"Error getting feedback {error}"
 
 @router.get("/feedbacks/filter")
-async def get_all_feedback_for_subject(userid:str,subject:str):
+async def get_all_feedback_for_subject(userid:Optional[str] = None ,subject:str):
     try:
         return get_all_feedback_subect_wise(userid,subject)
-
     except Exception as error:
         print(f"Error {error}")
         return f"Error getting feedback {error}"
 
 @router.get("/feedbacks/subjects")
-async def get_subjects_from_feedbacks(userid:str):
+async def get_subjects_from_feedbacks(userid:Optional[str]=None):
     try:
         return get_all_subjects_in_feedbacks(userid)
 
